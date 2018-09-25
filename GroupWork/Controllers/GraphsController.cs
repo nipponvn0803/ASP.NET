724596b2ddde31fcab4275a0b50a0016bb8e7813
Roberts Plorins <cros1404@gmail.com>
2018-09-25 10:29:03 +0300

    add Graphs page

---
+++ GroupWork/Controllers/GraphsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GroupWork.DAL;
+using GroupWork.Models;
+
+namespace GroupWork.Controllers
+{
+    public class GraphsController : Controller
+    {
+        private RecordContext db = new RecordContext();
+
+        // GET: Graphs
+        public ActionResult Index()
+        {
+            var records = db.Records.Include(r => r.User);
+            var x = Session["User"];
+            Session["User"] = "Roberts";
+            Console.Write(Session["User"]);
+            return View(records.ToList());
+        }
+
+        // GET: Graphs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Record record = db.Records.Find(id);
+            if (record == null)
+            {
+                return HttpNotFound();
+            }
+            return View(record);
+        }
+
+        // GET: Graphs/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserID = new SelectList(db.Users, "ID", "UserName");
+            return View();
+        }
+
+        // POST: Graphs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RecordID,UserID,Weight,Height,InputDate")] Record record)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Records.Add(record);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserID = new SelectList(db.Users, "ID", "UserName", record.UserID);
+            return View(record);
+        }
+
+        // GET: Graphs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Record record = db.Records.Find(id);
+            if (record == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserID = new SelectList(db.Users, "ID", "UserName", record.UserID);
+            return View(record);
+        }
+
+        // POST: Graphs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RecordID,UserID,Weight,Height,InputDate")] Record record)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(record).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserID = new SelectList(db.Users, "ID", "UserName", record.UserID);
+            return View(record);
+        }
+
+        // GET: Graphs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Record record = db.Records.Find(id);
+            if (record == null)
+            {
+                return HttpNotFound();
+            }
+            return View(record);
+        }
+
+        // POST: Graphs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Record record = db.Records.Find(id);
+            db.Records.Remove(record);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
